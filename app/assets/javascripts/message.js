7da1c915fce7235f9a1d97a8fd96253d23066474
yoshio9676 <yoshitarou0706@gmail.com>
2020-03-26 15:10:04 +0900

    message ajax 2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,12 +50,11 @@ $(function() {
       contentType: false
     })
       .done(function(data){
-        console.log(data);
         var html = buildHTML(data);
         $('.messages').append(html);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         $('#new_message')[0].reset();
-        $('.form__sent').prop('disabled', false)
+        $('.form__sent').prop('disabled', false);
       })
       .fail(function(){
         alert("メッセージの送信に失敗しました");
